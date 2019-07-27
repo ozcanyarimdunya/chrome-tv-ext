88b0f47dff5948dbc392161d813a15fb5c4c7d5e
ozcanyarimdunya <ozcanyd@gmail.com>
2019-07-27 13:23:41 +0300

    Update website

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.runtime.onInstalled.addListener(function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [
             new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'www.ecanlitvizle.live'},
+                pageUrl: {hostEquals: 'https://www.canlitv.la'},
             })
         ],
         actions: [
--- content.js
+++ content.js
@@ -1,25 +1,15 @@
-async function init() {
-	if (document.querySelector('footer') && document.querySelector('div.canlitv').nextSibling) {
-		document.querySelector('header.ortalama').parentNode.removeChild(document.querySelector('header.ortalama'))
-		document.querySelector('div.anaustrenk').parentNode.removeChild(document.querySelector('div.anaustrenk'))
-		document.querySelector('div.sag').parentNode.removeChild(document.querySelector('div.sag'))
-		document.querySelector('footer').parentNode.removeChild(document.querySelector('footer'))
-		document.querySelector('div.kanalbilgi').parentNode.removeChild(document.querySelector('div.kanalbilgi'));
-		document.querySelector('div.ilantahtasi').parentNode.removeChild(document.querySelector('div.ilantahtasi'))
-		document.querySelector('div.alternatif').parentNode.removeChild(document.querySelector('div.alternatif'))
-		document.querySelector('div.canlitv').nextSibling.remove()
+async function init(){
+	var yayinalani = document.querySelector('#yayinalani');
 
-		document.querySelectorAll('div.sol > :not(.canlitv)').forEach(el=> el.parentNode.removeChild(el))
-		document.querySelectorAll('div.clear').forEach(el=> el.parentNode.removeChild(el))
-		document.querySelectorAll('br').forEach(el=> el.parentNode.removeChild(el))
+	if (yayinalani) {
+		document.querySelector('#icerik').parentNode.removeChild(document.querySelector('#icerik'));
+		document.querySelector('.hakkinda').parentNode.removeChild(document.querySelector('.hakkinda'));
+		document.querySelector('.sag').parentNode.removeChild(document.querySelector('.sag'));
+		// document.querySelector('footer').parentNode.removeChild(document.querySelector('footer'));
+		document.querySelector('.various').parentNode.removeChild(document.querySelector('.various'));
+		document.querySelector('#dis').style.setProperty('box-shadow', '0px 0px 90px 0px #cac6c6');
 
-		document.querySelector('div.canlitv').style.setProperty('padding','0')
-		document.querySelector('div.kanallarliste').style.setProperty('padding','0')
-		document.querySelector('div.yayin').style.setProperty('height','596px')
-		document.querySelector('div#kanallarliste').style.setProperty('height','570px')
-		document.querySelector('div.yayinlar').style.setProperty('width','1140px')
-		document.querySelector('div.sol').style.setProperty('margin-left','5px')
-		document.querySelector('body').style.setProperty('overflow-y', 'hidden')
+		document.querySelector('.yazi').innerHTML = '<a href="http://semiworld.org/" target="_blank">semiworld.org</a>'
 	}else{
 		setTimeout(init, 100);
 	}
