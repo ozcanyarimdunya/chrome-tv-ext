4a783e8a8b6a62ef589f2e7b5c3a0d774112838a
ozcanyarimdunya <ozcanyd@gmail.com>
2019-07-29 01:11:59 +0300

    Update selected channel

--- content.js
+++ content.js
@@ -5,11 +5,14 @@ async function init(){
 		document.querySelector('#icerik').parentNode.removeChild(document.querySelector('#icerik'));
 		document.querySelector('.hakkinda').parentNode.removeChild(document.querySelector('.hakkinda'));
 		document.querySelector('.sag').parentNode.removeChild(document.querySelector('.sag'));
-		// document.querySelector('footer').parentNode.removeChild(document.querySelector('footer'));
 		document.querySelector('.various').parentNode.removeChild(document.querySelector('.various'));
 		document.querySelector('#dis').style.setProperty('box-shadow', '0px 0px 90px 0px #cac6c6');
-
-		document.querySelector('.yazi').innerHTML = '<a href="http://semiworld.org/" target="_blank">semiworld.org</a>'
+		document.querySelectorAll('ul.kanal li a').forEach((el) => {
+			if (el.getAttribute('href') === window.location.href) {
+				el.parentNode.style.setProperty('background-color', '#5f5d5d');
+				el.style.setProperty('color', '#d8971c')
+			}
+		})
 	}else{
 		setTimeout(init, 100);
 	}
