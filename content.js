a6e61be7cd4ab42cb3cee8e27fb7574617288d1c
o00598963 <ozcan.yarimdunya2@huawei.com>
2021-02-27 20:53:36 +0300

    Update links

--- content.js
+++ content.js
@@ -2,13 +2,11 @@ async function init(){
 	var yayinalani = document.querySelector('#yayinalani');
 
 	if (yayinalani) {
-		document.querySelector('.various').remove()
-		document.querySelector('#icerik').remove()
-		document.querySelector('.sag').remove()
-		document.querySelector('.hakkinda').remove()
-		document.querySelector('footer').remove()
-		document.querySelector('#st_gdpr_iframe').remove()
-		document.querySelector('#st-2').remove()
+		document.querySelector('.various').remove();
+		document.querySelector('#icerik').remove();
+		document.querySelector('.sag').remove();
+		document.querySelector('.hakkinda').remove();
+		document.querySelector('footer').remove();
 		document.querySelector('#dis').style.setProperty('box-shadow', '0px 0px 90px 0px #cac6c6');
 		document.querySelectorAll('ul.kanal li a').forEach((el) => {
 			if (el.getAttribute('href') === window.location.href) {
